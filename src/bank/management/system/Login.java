29eddc28c453021872f8a6aa4a8920ff6c95b1d1
Afrin-Jahan-Fatih <afrinjahan.bu.cse11@gmail.com>
2026-05-16 18:20:00 +0600

    checking

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,17 @@
+package bank.management.system;
+import javax.swing.*;
+
+public class Login extends JFrame{
+
+    Login(){
+        setSize(800,480);
+        setVisible(true);
+        setLocation(300,150);
+
+    }
+    public static void main(String[] args)
+    {
+        new Login();
+    }
+}
+
